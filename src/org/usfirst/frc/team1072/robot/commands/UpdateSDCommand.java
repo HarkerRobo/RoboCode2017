f04fa05207f92b93f28dd9e69afcfb75a33479ae
sanjay-rajasekharan <sanjay_rajasekharan@gmail.com>
2017-01-20 17:20:25 -0800

    ljkj
    Kl;

--- src/org/usfirst/frc/team1072/robot/commands/UpdateSDCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/UpdateSDCommand.java
@@ -9,16 +9,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class UpdateSDCommand extends Command {
 	private SmartDashboard sd;
-	private Encoder ec;
-    public UpdateSDCommand(Encoder e) {
+	private Encoder talon1;
+	private Encoder talon2;
+	private Encoder talon3;
+	private Encoder talon4;
+	
+    public UpdateSDCommand(Encoder talon1, Encoder talon2, Encoder talon3, Encoder talon4) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	sd = new SmartDashboard();
-    	ec = e;
+    	this.talon1 = talon1;
+    	this.talon2 = talon2;
+    	this.talon3 = talon3;
+    	this.talon4 = talon4;
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
