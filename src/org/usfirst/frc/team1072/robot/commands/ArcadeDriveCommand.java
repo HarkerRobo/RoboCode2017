d412d3d145fa42cb17d858dc3f01afd3087fd63e
Chaitanya Ravuri <chaitu.ravuri.10@gmail.com>
2017-01-30 18:08:08 -0800

    Arcade Drive Command
    yee

---
+++ src/org/usfirst/frc/team1072/robot/commands/ArcadeDriveCommand.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team1072.robot.commands;
+
+import org.usfirst.frc.team1072.robot.OI;
+import org.usfirst.frc.team1072.robot.Robot;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ArcadeDriveCommand extends Command {
+	
+	public ArcadeDriveCommand() {
+		requires(Robot.drivetrain);
+	}
+
+	protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double x = OI.controller.getX(Hand.kRight);
+    	double y = OI.controller.getY(Hand.kRight);
+    	Robot.drivetrain.tankDrive(y*(1+x/2),y*(1-x/2));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.drivetrain.tankDrive(0, 0);
+    }
+
+}
