137069b0a8c9dc5d874f1235d52e53c6ae3a44e5
Ashwin Reddy <ashwin.reddy338@gmail.com>
2017-02-04 18:40:07 -0800

    Added SillyDash Main file

---
+++ src/org/usfirst/frc/team1072/sillyDashboard/Main.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team1072.sillyDashboard;
+import org.usfirst.frc.team1072.shared.Client;
+/**
+ * 
+ * @author Ashwin Reddy
+ *
+ */
+public class Main {
+	public static final String raspiIP = "";
+	
+	
+	public static void main(String[] args) {
+		new Client();
+	}
+}
