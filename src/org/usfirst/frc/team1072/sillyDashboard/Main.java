4e944d06f6233d78a4644858816408018c8d1d89
Ashwin Reddy <ashwin.reddy338@gmail.com>
2017-02-05 02:33:07 -0800

    Minor naming changes

--- src/org/usfirst/frc/team1072/sillyDashboard/Main.java
+++ src/org/usfirst/frc/team1072/sillyDashboard/Main.java
@@ -28,29 +28,29 @@ public class Main {
 	private static Pipeline pipe;
 	private static int width = 640;
 	private static int height = 480;
-	
+    private static String title = "Prof. Reddy's Wonderful Silly Dashboard Copy+Paste Program";
+
 	public static RaspiNetworker raspinet;
 	public static void main(String[] args) {
 		raspinet = new RaspiNetworker();
 		raspinet.run();
 		raspinet.send(new JSONObject());
-		Gst.init("CameraTest", args);
+		Gst.init("RaspiCamera", args);
         EventQueue.invokeLater(new Runnable() {
-//        	public Pipeline pipe;
             @Override
             public void run() {
                 SimpleVideoComponent vc = new SimpleVideoComponent();
                 Bin bin = Bin.launch("udpsrc port=5001 ! application/x-rtp, payload=96 ! rtph264depay ! avdec_h264 ! videoconvert", true);
                 pipe = new Pipeline();
                 pipe.addMany(bin, vc.getElement());
-                Pipeline.linkMany(bin, vc.getElement());           
+                Pipeline.linkMany(bin, vc.getElement());
 
-                JFrame f = new JFrame("Professor Reddy's Wonderful Copy+Paste Program");
+                JFrame f = new JFrame(title);
                 f.add(vc);
-                vc.setPreferredSize(new Dimension(640, 480));
+                vc.setPreferredSize(new Dimension(width, height));
                 f.pack();
                 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
+
                 pipe.play();
                 f.setVisible(true);
             }
