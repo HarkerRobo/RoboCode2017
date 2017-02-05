19da94333539df5ded3cd1e734b24e529ebeb6fa
Ashwin Reddy <ashwin.reddy338@gmail.com>
2017-02-04 18:52:18 -0800

    Added important Silly Dashboard things
    Restructured plus basic code

--- src/org/usfirst/frc/team1072/sillyDashboard/Main.java
+++ src/org/usfirst/frc/team1072/sillyDashboard/Main.java
@@ -1,4 +1,6 @@
 package org.usfirst.frc.team1072.sillyDashboard;
+import org.usfirst.frc.team1072.shared.Networking;
+import org.json.JSONObject;
 import org.usfirst.frc.team1072.shared.Client;
 /**
  * 
@@ -6,10 +8,12 @@ import org.usfirst.frc.team1072.shared.Client;
  *
  */
 public class Main {
-	public static final String raspiIP = "";
-	
-	
+//	public static final String raspiIP = Networking.RASPI_IP;
+//	public static final int port = 0;
+	public static RaspiNetworker raspinet;
 	public static void main(String[] args) {
-		new Client();
+		raspinet = new RaspiNetworker();
+		raspinet.run();
+		raspinet.send(new JSONObject());
 	}
 }
