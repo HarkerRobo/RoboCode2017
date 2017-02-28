6bc30cd2070c3d45d1b9db786a9daf9a240f847b
Joel Manning <stopwatchj@gmail.com>
2017-02-27 16:39:14 -0800

    Edited autonomous 2

--- src/org/usfirst/frc/team1072/robot/commands/AutonomousGear2CommandGroup.java
+++ src/org/usfirst/frc/team1072/robot/commands/AutonomousGear2CommandGroup.java
@@ -17,35 +17,6 @@ public class AutonomousGear2CommandGroup extends CommandGroup {
     	addSequential(new MoveDistanceCommand(gearTwoDistance));
     	addSequential(new AutonPusherCommand());
     	addSequential(new MoveDistanceCommand(-24));
-    	System.out.println("Constructed 2");
+    	//TODO cross the line
     }
-
-	/* (non-Javadoc)
-	 * @see edu.wpi.first.wpilibj.command.CommandGroup#initialize()
-	 */
-	@Override
-	protected void initialize() {
-		super.initialize();
-		System.out.println("Initialized 2");
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.wpi.first.wpilibj.command.CommandGroup#execute()
-	 */
-	@Override
-	protected void execute() {
-		super.execute();
-		System.out.println("Executed 2");
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.wpi.first.wpilibj.command.CommandGroup#end()
-	 */
-	@Override
-	protected void end() {
-		super.end();
-		System.out.println("Ended 2");
-	}
-	
-	
 }
