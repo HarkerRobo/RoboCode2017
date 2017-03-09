b9a41346dc8f9396f0c2f9b28a8df7de709471e7
Joel Manning <stopwatchj@gmail.com>
2017-03-08 16:29:45 -0800

    Updated Autonomous Values

--- src/org/usfirst/frc/team1072/robot/commands/AutonomousCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/AutonomousCommand.java
@@ -7,12 +7,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class AutonomousCommand extends CommandGroup {
 
-	public static final AutonomousCommand RED_LEFT = new AutonomousCommand(77, 60, 55.5);
-	public static final AutonomousCommand RED_CENTER = new AutonomousCommand(89, 0, 0);
-	public static final AutonomousCommand RED_RIGHT = new AutonomousCommand(88.3, -60, 28.7);
-	public static final AutonomousCommand BLUE_LEFT = new AutonomousCommand(88.3, 60, 28.7);
-	public static final AutonomousCommand BLUE_CENTER = new AutonomousCommand(89, 0, 0);
-	public static final AutonomousCommand BLUE_RIGHT = new AutonomousCommand(77, -60, 55.5);
+	public static final AutonomousCommand RED_LEFT = new AutonomousCommand(74.946, 60, 55.45);
+	public static final AutonomousCommand RED_CENTER = new AutonomousCommand(53.136, 0, 0);
+	public static final AutonomousCommand RED_RIGHT = new AutonomousCommand(88.34, -60, 28.718);
+	public static final AutonomousCommand BLUE_LEFT = new AutonomousCommand(88.34, 60, 28.718);
+	public static final AutonomousCommand BLUE_CENTER = new AutonomousCommand(53.136, 0, 0);
+	public static final AutonomousCommand BLUE_RIGHT = new AutonomousCommand(74.946, -60, 55.45);
 	
     private AutonomousCommand(double initialDistance, double turn, double secondDistance) {
         addSequential(new DriveDistanceTimed(initialDistance));
